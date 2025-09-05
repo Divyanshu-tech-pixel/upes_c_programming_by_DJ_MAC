bb4776d3e59a40e8e34fbbd946974b30edc0bae9
Divyanshu-tech-pixel <https://github.com/Divyanshu-tech-pixel/upes_c_programming_by_DJ_MAC.git>
2025-09-05 22:11:15 +0530

    Day 1 code

---
+++ day1.c
@@ -0,0 +1,10 @@
+/*Write a program to input two number and display their sum*/
+#include<stdio.h> //Header file
+int main()
+{
+    float i,x;
+    printf("Enter two number");
+    scanf("%f %f",&i,&x);
+    printf("sum is %f",i+x);
+    return 0;
+}
